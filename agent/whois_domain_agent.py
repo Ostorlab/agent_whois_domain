9596c406184a63bfd776f3be421b4468456417ec
deadly-panda <abderrahim.haddadi@ostorlab.dev>
2023-01-23 11:20:09 +0100

    Add domain regex scope.

--- agent/whois_domain_agent.py
+++ agent/whois_domain_agent.py
@@ -1,5 +1,7 @@
 """Whois Domain Agent: Agent responsible for retrieving WHOIS information of a domain."""
 import logging
+import re
+from typing import Optional
 
 from rich import logging as rich_logging
 import whois
@@ -36,6 +38,7 @@ class AgentWhoisDomain(agent.Agent, persist_mixin.AgentPersistMixin):
     ) -> None:
         agent.Agent.__init__(self, agent_definition, agent_settings)
         persist_mixin.AgentPersistMixin.__init__(self, agent_settings)
+        self._scope_domain_regex: Optional[str] = self.args.get("scope_domain_regex")
 
     def process(self, message: msg.Message) -> None:
         """Starts a whois scan, wait for the scan to finish,
@@ -49,14 +52,33 @@ class AgentWhoisDomain(agent.Agent, persist_mixin.AgentPersistMixin):
             return
 
         logger.info("Processing message of selector : %s", message.selector)
-        if not self.set_add("agent_whois_domain_asset", domain):
+        if self.set_add("agent_whois_domain_asset", domain) is False:
             logger.info("target %s was processed before, exiting", domain)
             return
+        if self._is_domain_in_scope(self._scope_domain_regex, domain) is False:
+            logger.warning(
+                "Domain %s is not in scanning scope %s",
+                domain,
+                self._scope_domain_regex,
+            )
+            return
+
         try:
             scan_output = self._fetch_whois(domain)
             self._emit_result(scan_output)
         except parser.PywhoisError as e:
             logger.error(e)
+
+    def _is_domain_in_scope(
+        self, scope_domain_regex: Optional[str], domain: str
+    ) -> bool:
+        if scope_domain_regex is None:
+            return True
+        domain_in_scope = re.match(scope_domain_regex, domain) is not None
+        if not domain_in_scope:
+            return False
+        else:
+            return False
 
     def _fetch_whois(self, domain_name: str) -> whois.parser.WhoisCom:
         """Collect whois data.
